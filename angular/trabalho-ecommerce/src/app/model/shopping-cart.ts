e8fe117555538550a4d99b0c750465ede8bfca92
Mario <mariohj94@gmail.com>
2019-01-23 20:36:48 -0200

    Alterações funcionalidades carrinho de compras

--- angular/trabalho-ecommerce/src/app/model/shopping-cart.ts
+++ angular/trabalho-ecommerce/src/app/model/shopping-cart.ts
@@ -1,6 +1,7 @@
 import { Product } from './products';
 export class ShoppingCart{
     constructor(
+        public id:string,
         public product:Product,
         public amount:number
     ){}
--- angular/trabalho-ecommerce/src/app/pages/shopping-cart/shopping-cart.component.ts
+++ angular/trabalho-ecommerce/src/app/pages/shopping-cart/shopping-cart.component.ts
@@ -17,6 +17,7 @@ export class ShoppingCartComponent implements OnInit {
   listShoppingCarts:Array<ShoppingCart>;
 
   valorTotal:number = 0;
+  qtdeItens:number= 0;
 
   constructor(
     private shoppingCartService:ShoppingCartService
@@ -29,15 +30,35 @@ export class ShoppingCartComponent implements OnInit {
   listar(){
     this.shoppingCartService.findShoppingCarts().subscribe((list:Array<ShoppingCart>)=>{
       this.listShoppingCarts = list;
-      this.calculaValorTotal(this.listShoppingCarts);
+      this.calculaTotais(this.listShoppingCarts);
     },err=>{
 
     });
   }
 
-  calculaValorTotal(list:Array<ShoppingCart>){
+  calculaTotais(list:Array<ShoppingCart>){
+    this.valorTotal = 0;
+    this.qtdeItens = 0;
     for(let shopp of list){
       this.valorTotal+=(shopp.product.price *shopp.amount);
+      this.qtdeItens+=shopp.amount;
+    }
+  }
+
+  removeListShopp(shopp:ShoppingCart){
+    this.listShoppingCarts = this.listShoppingCarts.filter(obj => obj !== shopp);
+    this.calculaTotais(this.listShoppingCarts);
+  }
+
+  plusAmount(shopp:ShoppingCart){
+    this.listShoppingCarts.find(obj => obj == obj).amount = shopp.amount+1;
+    this.calculaTotais(this.listShoppingCarts);
+  }
+
+  minAmount(shopp:ShoppingCart){
+    if(shopp.amount>1){
+      this.listShoppingCarts.find(obj => obj == obj).amount = shopp.amount-1;
+      this.calculaTotais(this.listShoppingCarts);
     }
   }
 }
