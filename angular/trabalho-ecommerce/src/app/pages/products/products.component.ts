9cceccfc7e68f862b08de6a7ec498573d3480b78
Fernando Souza Lopes <fernando.slopes19@gmail.com>
2019-01-27 23:32:11 -0200

    Atualização das categorias dos produtos

--- angular/trabalho-ecommerce/src/app/pages/products/products.component.ts
+++ angular/trabalho-ecommerce/src/app/pages/products/products.component.ts
@@ -1,38 +1,67 @@
-import { Component, OnInit } from "@angular/core";
-import { ProductsService } from "./products.service";
-import { Product } from "../../model/product.model";
-import { IMG_PRODUCTS } from "../../app.api";
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from './products.service';
+import { Product } from '../../model/product.model';
+import { IMG_PRODUCTS } from '../../app.api';
 
 @Component({
-  selector: "app-products",
-  templateUrl: "./products.component.html",
-  styleUrls: ["./products.component.css"]
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
   products: Product[];
+  listProductByCategory: Product[] = [];
+
+  categories = ['TODOS', 'Eletrodoméstico',
+    'Ar-Condicionado',
+    'Ventilador',
+    'Liquidificador',
+    'TV',
+    'Monitor',
+    'Smartphone',
+    'Cafeteira',
+    'Fritadeira'];
+
+  selectedCategory = 'TODOS';
 
   imagePath: string = IMG_PRODUCTS;
 
-  constructor(private productsService: ProductsService) {}
+  constructor(private productsService: ProductsService) { }
 
   ngOnInit() {
     this.productsService.products().subscribe(products => {
       this.products = products;
       this.products.forEach(
-        element => (element.imageUrl = this.imagePath + element.imageUrl)
+        (element: Product) => {
+          element.imageUrl = this.imagePath + element.imageUrl;
+          this.listProductByCategory.push(element);
+        }
       );
     });
   }
 
+
+
+
+
   productsByCategory(category: string): Product[] {
-    const listProductByCategory: Product[] = [];
+    this.selectedCategory = category;
 
-    this.products.forEach(element => {
-      if (element.category === category) {
-        listProductByCategory.push(element);
-      }
-    });
+    this.listProductByCategory = [];
 
-    return listProductByCategory;
+    if (category === 'TODOS') {
+      this.products.forEach(element => {
+        this.listProductByCategory.push(element);
+      });
+    } else {
+      this.products.forEach(element => {
+        if (element.category === category) {
+          this.listProductByCategory.push(element);
+        }
+      });
+
+      return this.listProductByCategory;
+    }
+
   }
 }
